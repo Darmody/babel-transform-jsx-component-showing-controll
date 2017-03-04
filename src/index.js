2a8afcdb09381a75a9e804bcdce7e65a325462f8
Darmody <eterlf41@gmail.com>
2017-03-04 22:19:22 +0800

    1.0.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const isInload = attribute => attribute.type === 'JSXAttribute' && attribute.name.name === 'in-load'
-const isntInload = x => !isInload(x)
+const isShow = attribute => attribute.type === 'JSXAttribute' && attribute.name.name === 'is-show'
+const isntShow = x => !isShow(x)
+const isCustomComponent = attribute => attribute.type === 'JSXAttribute' && attribute.name.name === 'show-tag'
 
 export default ({ types: t }) => ({
   visitor: {
@@ -7,19 +8,19 @@ export default ({ types: t }) => ({
       const { node } = path
       const attributes = node.openingElement.attributes
       if (!attributes) return
-      const ifAttribute = attributes.filter(isInload)[0]
+      const ifAttribute = attributes.filter(isShow)[0]
       if (ifAttribute) {
-        const opening = t.JSXOpeningElement(node.openingElement.name, attributes.filter(isntInload))
+        const opening = t.JSXOpeningElement(node.openingElement.name, attributes.filter(isntShow))
         const tag = t.JSXElement(opening, node.closingElement, node.children)
 
-        const inLoading = t.JSXOpeningElement(t.JSXIdentifier('div'), [
-          t.JSXAttribute(t.JSXIdentifier('className'), t.StringLiteral('in-loading'))
-        ])
-        const CloseInLoading = t.JSXClosingElement(t.JSXIdentifier('div'))
-        const loadingTag = t.JSXElement(inLoading, CloseInLoading, [])
+        const customComponentAttribute = attributes.filter(isCustomComponent)[0]
+        tagName = customComponentAttribute ? customComponentAttribute.value : 'InLoading'
+        const inShowing = t.JSXOpeningElement(t.JSXIdentifier(tagName), [])
+        const CloseinShowing = t.JSXClosingElement(t.JSXIdentifier(tagName))
+        const showingTag = t.JSXElement(inShowing, CloseinShowing, [])
 
         const conditional = t.conditionalExpression(
-          ifAttribute.value.expression, loadingTag, tag,
+          ifAttribute.value.expression, showingTag, tag,
         )
         path.replaceWith(conditional)
       }
